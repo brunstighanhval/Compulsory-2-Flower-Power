89010f99f9f94a1ebe10049b67b01283cb6bca6b
brunstighanhval <mlkaer1@hotmail.com>
2023-03-21 10:18:20 +0100

    masd

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,9 @@ public class Main extends Application {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("/GUI/View/EventView.fxml"));
         Parent root = loader.load();
+        primaryStage.setScene(new Scene(root));
+        primaryStage.getScene().getStylesheets().add(getClass().getResource("CSS/Event.css").toExternalForm());
+
 
 
         primaryStage.setTitle("Event Tickets EASV Bar");
