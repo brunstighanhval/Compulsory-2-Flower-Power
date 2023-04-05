61095de7450604d0a1cf4bdb8077bb2d10b7b2b8
brunstighanhval <113671720+brunstighanhval@users.noreply.github.com>
2023-04-05 09:09:47 +0200

    sadf

--- src/GUI/Controller/CreateTicketController.java
+++ src/GUI/Controller/CreateTicketController.java
@@ -75,9 +75,9 @@ public class CreateTicketController extends BaseController implements Initializa
 
         Properties properties = new Properties();
         properties.put("mail.smtp.host", host);
-        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.port", "25");
         properties.put("mail.smtp.auth", "true");
-        //properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
         Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(user, password);
